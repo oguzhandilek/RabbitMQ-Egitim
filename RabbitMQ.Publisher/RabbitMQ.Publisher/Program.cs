be082ea14d3d8000478adaaf83c53e4d7115a8e5
oguzhandilek <oguzhandilek25@gmail.com>
2026-06-17 16:34:39 +0300

    first-commit

---
+++ RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,15 @@
+﻿
+#region RabbitMQ.Client Kütüphanesi
+//.NET teknolojileri ile RabbitMQ'yu kullanabilmek için RabbitMQ.Client kütüphanesini projenize yüklemeniz gerekmektedir.
+#endregion
+#region Publisher Uygulaması İşlem Sırası
+//1. Bağlantı Oluşturma
+//RabbitMQ SUnUCUSUna bağlantı oluşturunuz.
+//2.Bağlantıyı Aktifleştirme ve Kanal Açma
+//Bağlantıyı aktifleştiriniz ve ardından bu bağlantı üzerinden işlemleri yürütebilmek için bir kanal açınız.
+//3.Queue Oluşturma
+//Mesajların gönderileceği kuyruğu oluşturunuz.
+//4.Queue'ya Mesaj Gönderme
+//Kuyruğa mesaj gönderiniz.
+#endregion
+Console.WriteLine();
